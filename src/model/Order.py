7f3ebcc6d342b34effc6eb8f2a805f9394e5702e
ChrisChV <xnpiochv@gmail.com>
2019-05-28 12:52:35 -0500

    Oredr.py added

--- src/model/Order.py
+++ src/model/Order.py
@@ -1,20 +1,39 @@
 import Utils.sad as sad
 import time
+from datetime import datetime
+
 
 class Order:
-    def __init__(self, price, order_type, transaction_id, state):
+    def __init__(self):
         self.id = None
+        self.price = None
+        self.order_type = None
+        self.transaction_id = None
+        self.state = None
+
+    def create(self, db, price, order_type, transaction_id, state):
         self.price = price
         self.order_type = order_type
         self.transaction_id = transaction_id
         self.state = state
-    
-    def create(self, db):
         order = db.insert(sad._ORDER_TABLE_NAME_, {sad._TRANSACTION_ID_COL_NAME_:self.transaction_id, 
                                             sad._PRICE_COL_NAME_: self.price,
                                             sad._TYPE_COL_NAME_: self.order_type,
                                             sad._STATE_COL_NAME_: self.state}, returning='order_id')
         self.id = order['order_id']
         db.insert(sad._DATES_TABLE_NAME_, {sad._ORDER_ID_COL_NAME_: self.id,
-                                            sad._INIT_DATE_COL_NAME_: int(time.time())})
+                                            sad._INIT_DATE_COL_NAME_: datetime.now()})
         
+
+
+def listToOrders(listOfOrders, transaction_id):
+    orders = {}
+    for order in listOfOrders:
+        _order = Order()
+        _order.id = order[sad._ORDER_ID_COL_NAME_]
+        _order.price = order[sad._PRICE_COL_NAME_]
+        _order.order_type = order[sad._TYPE_COL_NAME_]
+        _order.transaction_id = transaction_id
+        _order.state = order[sad._STATE_COL_NAME_]
+        orders[_order.order_type] = _order
+    return orders
--- src/model/Transaction.py
+++ src/model/Transaction.py
@@ -1,4 +1,5 @@
 import Utils.sad as sad
+import Binance.Binance_Wrapper as BW
 import Order
 import bd
 
@@ -10,22 +11,33 @@ class Transaction:
         self.orders = None
 
     def create(self, symbol, entry, lose, profit):
-        ##symbol tiene que existir
-        ##entry tiene que ser menor que el precio actual
+        if not BW.symbolExists(symbol):
+            return False, "Symbol doesn't exist"        
+        actualPrice = float(BW.getPrice(symbol))
+        if entry > actualPrice:
+            return False, "The entry price has to be less than actual price (" + str(actualPrice) + ")"
         if lose >= entry:
-            return False, "The lose price has to be grater than entry price"
+            return False, "The lose price has to be less than entry price"
         if profit <= entry:
-            return False, "The profit price has to be less than entry price"
+            return False, "The profit price has to be grater than entry price"
         self.symbol = symbol
         db = bd.BD.getConn()
         transaction = db.insert(sad._TRANSACTION_TABLE_NAME_, {sad._SYMBOL_COL_NAME_: self.symbol}, returning='transaction_id')
         self.id = transaction['transaction_id']
-        entryOrder = Order.Order(entry, sad._ENTRY_TYPE_, self.id, sad._WAITING_STATE_)
-        loseOrder = Order.Order(lose, sad._LOSE_TYPE_, self.id, sad._INIT_STATE_)
-        profitOrder = Order.Order(profit, sad._PROFIT_TYPE_, self.id, sad._INIT_STATE_)
-        entryOrder.create(db)
-        loseOrder.create(db)
-        profitOrder.create(db)
+        entryOrder = Order.Order()
+        loseOrder = Order.Order()
+        profitOrder = Order.Order()
+        entryOrder.create(db, entry, sad._ENTRY_TYPE_, self.id, sad._WAITING_STATE_)
+        loseOrder.create(db, lose, sad._LOSE_TYPE_, self.id, sad._INIT_STATE_)
+        profitOrder.create(db, profit, sad._PROFIT_TYPE_, self.id, sad._INIT_STATE_)
         db.commit()
+        self.orders = {sad._ENTRY_TYPE_:entryOrder, sad._LOSE_TYPE_:loseOrder, sad._PROFIT_TYPE_:profitOrder}
         return True, None
-        
+    
+    def get(self, transaction_id):
+        self.id = transaction_id
+        db = bd.BD.getConn()
+        transaction = db.fetchone(sad._TRANSACTION_TABLE_NAME_, fields=['symbol'], where=('id=%s', str(self.id)))
+        self.symbol = transaction['symbol']
+        orders = db.fetchall(sad._ORDER_TABLE_NAME_, fields=['order_id', 'price', 'type', 'state'], where=("transaction_id=%s", str(self.id)))
+        self.orders = Order.listToOrders(orders, self.id)
